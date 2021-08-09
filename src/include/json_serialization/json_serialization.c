8962cc334e3a656bedf5b9c0aaad1da5aa3663b0
FallAngel1337 <fallangel@protonmail.com>
2021-08-09 14:12:02 -0300

    json_serialization.c: defining jsonSerialize function

---
+++ src/include/json_serialization/json_serialization.c
@@ -0,0 +1,37 @@
+#include "json_serialization.h"
+
+#include <cjson/cJSON.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+char *jsonSerialize(struct file_info *_file_info)
+{
+    cJSON *file = cJSON_CreateObject();
+    if (!file) {
+        return NULL;
+    }
+
+    cJSON *file_name = NULL;
+    cJSON *file_size = NULL;
+    cJSON *checksum = NULL;
+    char *json = NULL;
+
+    file_name = cJSON_CreateString(_file_info->file_name);
+    file_size = cJSON_CreateNumber(_file_info->file_size);
+    checksum = cJSON_CreateRaw(_file_info->checksum);
+
+    cJSON_AddItemToObject(file, "file_name", file_name);
+    cJSON_AddItemToObject(file, "file_size", file_size);
+    cJSON_AddItemToObject(file, "file_name", file_name);
+    
+    json = cJSON_Print(file);
+    cJSON_Delete(file);
+    return json;
+}
+
+struct file_info* jsonDeserialize(const char *json)
+{
+    return NULL;
+}
