e427979e1cdc799e97addb602f3c819f4695351e
FallAngel1337 <fallangel@protonmail.com>
2021-08-09 09:10:15 -0300

    file_info.c: create the init funcion

--- src/include/file_info/file_info.c
+++ src/include/file_info/file_info.c
@@ -3,17 +3,28 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <openssl/sha.h>
 
 struct file_info {
     char *file_name;
     char *file_type;
-    size_t file_size;
+    size_t file_size; // in bytes
 
-    unsigned char *checksum;
+    unsigned char *checksum; // sha256
 };
 
 int file_info_init(struct file_info *_file_info)
 {
+    if (!_file_info) return -1;
+
+    *_file_info = (struct file_info) {
+        .file_name = NULL,
+        .file_type = NULL,
+        .file_size = 0,
+        .checksum = NULL,
+    };
+
     return 0;
 }
 
