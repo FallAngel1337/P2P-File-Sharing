bb3c8518d25d588c8f4fde4cf6d34c75138ce148
FallAngel1337 <fallangel@protonmail.com>
2021-08-13 13:54:17 -0300

    p2p-client: adding configuration support

--- src/client/p2p-client.c
+++ src/client/p2p-client.c
@@ -12,12 +12,17 @@
  * Also serve the file for downloading.
 */
 
+// Global headers
 #include "../include/file_info/file_info.h"
 #include "../include/json_serialization/json_serialization.h"
+
+// Client headers
+#include "include/config/config.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <libconfig.h>
 #include <stdbool.h>
 
 static char* get_file_ext(const char *__restrict__ _filename)
@@ -28,7 +33,7 @@ static char* get_file_ext(const char *__restrict__ _filename)
     return __filename;
 }
 
-static inline bool isa_torrent(const char *_filename)
+static inline bool is_a_torrent(const char *_filename)
 {
     char *_ext = get_file_ext(_filename);
     return (strcmp(_ext, "torrent")) ? false : true;
@@ -41,22 +46,39 @@ int main(int argc, char **argv)
         return -1;
     }
 
-    struct file_info fileinfo;
-    char *torrent_file;
-    file_info_init(&fileinfo);
+    config_t conf;
+    const char *CLIENT_IP;
+    unsigned CLIENT_PORT;
+    const char *CLIENT_LOG_DIR;
 
-    if (isa_torrent(argv[1]))
-    {
-        /* Request server ... */
-        jsonReadFile(argv[1], &fileinfo);
-        printf("Filename: %s\n", fileinfo.file_name);
-        printf("File size: %zu\n", fileinfo.file_size);
-        printf("Checksum: %s\n", fileinfo.checksum);
-    } else {
-        file_info_load(argv[1], &fileinfo);
-        jsonWriteFile(&torrent_file, &fileinfo);
-        printf("Created %s\n", torrent_file);
-        free(torrent_file);
+    const char *CSERVER_IP;
+    unsigned CSERVER_PORT;
+
+    const char *filename = argv[1];
+
+    config_init(&conf);
+
+    if (config_read_file(&conf, CLIENT_CONFIG_FILE) == CONFIG_FALSE) {
+        fprintf(stderr, "Can't load the configuration file %s :: %s\n", CLIENT_CONFIG_FILE, config_error_text(&conf));
+        config_destroy(&conf);
+        return -1;
     }
+
+
+    config_lookup_string(&conf, "CLIENT_IP", &CLIENT_IP);
+    config_lookup_int(&conf, "CLIENT_PORT", &CLIENT_PORT);
+    config_lookup_string(&conf, "CLIENT_LOG_DIR", &CLIENT_LOG_DIR);
+    
+    config_lookup_string(&conf, "CSERVER_IP", &CSERVER_IP);
+    config_lookup_int(&conf, "CSERVER_PORT", &CSERVER_PORT);
+
+    if (is_a_torrent(filename)) {
+        printf("It's a torrent!");
+    } else {
+        printf("It's not a torrent!");
+    }
+
+
+    config_destroy(&conf);
     return 0;
 }
