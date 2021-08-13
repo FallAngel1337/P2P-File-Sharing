340cd23244eeea7e3479195524427bd60315ab2e
FallAngel1337 <fallangel@protonmail.com>
2021-08-13 13:56:49 -0300

    added client libraries

---
+++ src/client/include/config/config.h
@@ -0,0 +1,14 @@
+/**
+ * config.h - Client configuration header
+ * Author: FallAngel
+ * Date: 2021-08-13
+ * 
+ * Copyright (c) 2021
+ */
+
+#ifndef _CONFIG_H
+#define _CONFIG_H
+
+#define CLIENT_CONFIG_FILE "/etc/p2pd/client.conf"
+
+#endif /* _CONFIG_H */
