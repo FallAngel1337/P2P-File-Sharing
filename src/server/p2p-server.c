fe3e6700a8aa7acf23b3c3295b644a752ba768ae
FallAngel1337 <fallangel@protonmail.com>
2021-08-14 16:50:43 -0300

    Adding tehe server side

---
+++ src/server/p2p-server.c
@@ -0,0 +1,20 @@
+/**
+ * p2p-server.c - P2P server
+ * Author: FallAngel
+ * Date: 2021-08-14
+ * 
+ * Copyright (c) 2021
+ */
+/**
+ * Will answer the client with
+ * the file owners.
+*/
+
+// Global headers
+#include "../include/node/node.h"
+#include "../include/json_serialization/json_serialization.h"
+
+// Server headers
+#include "include/config/config.h"
+
+int main(void) {return 0;}
