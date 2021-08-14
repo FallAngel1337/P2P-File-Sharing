285f87775b87a199244f521daa45d93f7ad2abe5
FallAngel1337 <fallangel@protonmail.com>
2021-08-14 14:31:12 -0300

    minor fix

--- src/client/include/net/connection.c
+++ src/client/include/net/connection.c
@@ -23,7 +23,7 @@ int connecttos(const char *__restrict__ ip, uint32_t port,
     addr.sin_addr.s_addr = htonl(inet_addr(ip));
     addr.sin_port = htons(port);
 
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0) < 0)) {
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         fprintf(stderr, "Was not possible to create the socket: %s\n", strerror(errno));
         return -1;
     }
