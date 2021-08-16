9f536cf8ce8e3e02d4de1a9251cef6be392f64ba
FallAngel1337 <fallangel@protonmail.com>
2021-08-16 14:39:11 -0300

    minor changes #5

--- src/include/logging/logging.c
+++ src/include/logging/logging.c
@@ -1,3 +1,5 @@
+#define _GNU_SOURCE
+
 #include "logging.h"
 
 #include <string.h>
@@ -6,24 +8,26 @@
 #include <stdarg.h>
 #include <time.h>
 
-#define MAX_BUFF 1024
+#define MAX_BUFF 100
 
 void _log(const char *__restrict__ _log_file , const char *__restrict__ _level, const char *_file, int _line, char *__restrict__ _format, ...)
 {
     time_t _t;
     va_list va;
     va_start(va, _format);
-    char *format = calloc(1, MAX_BUFF);
+    char *date = calloc(1, MAX_BUFF);
+    char *format;
     time(&_t);
     struct tm *datetime = localtime(&_t);
     
     // pretty messy ... but works ;)
-    size_t _len = strftime(format, MAX_BUFF, "%x %H:%M:%S ", datetime);
-    (!strcmp(_level, ERROR)) ? snprintf(format+_len, MAX_BUFF, "%s %s:%d - %s", _level, _file, _line, _format) : 
-                               snprintf(format+_len, MAX_BUFF, "%s - %s", _level, _format);
+    size_t _len = strftime(date, MAX_BUFF, "%x %H:%M:%S ", datetime);
+    (!strcmp(_level, ERROR)) ? asprintf(&format, "%s %s %s:%d - %s", date, _level, _file, _line, _format) : 
+                               asprintf(&format, "%s %s - %s", date, _level, _format);
 
     FILE *fp = fopen(_log_file, "a+");
     vfprintf(fp, format, va);
     fclose(fp);
+    free(date);
     free(format);
 }
