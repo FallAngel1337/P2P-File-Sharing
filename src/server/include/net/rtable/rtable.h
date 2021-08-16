c3f9b2a9b0dd415cf99eb7eea3af39867df8a6ce
FallAngel1337 <fallangel@protonmail.com>
2021-08-16 08:50:50 -0300

    p2p-server: adding rtable

---
+++ src/server/include/net/rtable/rtable.h
@@ -0,0 +1,22 @@
+/**
+ * rtable.h - Server's routing table implementation
+ * Author: FallAngel
+ * Date: 2021-08-16
+ * 
+ * Copyright (c) 2021
+ */
+
+#ifndef _RTABLE_H
+#define _RTABLE_H
+
+#include "../../../../include/file_info/file_info.h"
+#include "../../../../include/json_serialization/json_serialization.h"
+
+#define TABLE_MAX 10
+
+struct rtable {
+    struct file_info *_file_info;
+    struct rtable *next;
+};
+
+#endif /* _RTABLE_H */
