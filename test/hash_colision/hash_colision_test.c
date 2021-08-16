f7879c56075b9a93f0ab9783d4ebe48962c4947e
FallAngel1337 <fallangel@protonmail.com>
2021-08-16 18:24:56 -0300

    minor changes #6

--- test/hash_colision/hash_colision_test.c
+++ test/hash_colision/hash_colision_test.c
@@ -9,19 +9,19 @@ void TestColision(CuTest *tc)
     printf("Testing hash colision ...\n");
     int colided = 0;
     float colidedpercent = 0;
-    int rep[10000], ok = 0;
+    int rep[1000], ok = 0;
     memset(rep, 0, sizeof(rep));
     
-    for (int i=0; i < 10000; i++) {
-        uint64_t n = mulhashing(i, 10000);
-        for (int j=0; j < 10000; j++) {
+    for (int i=0; i < 1000; i++) {
+        uint64_t n = mulhashing(i, 1000);
+        for (int j=0; j < 1000; j++) {
             if (n == rep[j]) colided++;
         }
         rep[i] = n;
     }
 
-    colidedpercent = colided/10000;
-    printf("Total: 10000\nColided: %d\nColided%%: %.2f%%\n", colided, colidedpercent);
+    colidedpercent = (colided-1000)/100;
+    printf("Total: 1000\nColided: %d\nColided%%: %.2f%%\n", colided, colidedpercent);
     if (colidedpercent > 5) ok = 1;
 
     CuAssertIntEquals(tc, 0, ok);
