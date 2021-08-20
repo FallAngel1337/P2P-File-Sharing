11831cdc865ed767f17154bdd73db45ee795ad3e
FallAngel <62574781+FallAngel1337@users.noreply.github.com>
2021-08-19 21:53:00 -0300

    Update node_serr.h
    Fix the header name in the doc comments and at other places

--- src/include/node/serr/node_serr.h
+++ src/include/node/serr/node_serr.h
@@ -1,12 +1,12 @@
 /**
- * json_serialization.h - (De)Serialize a POD into a JSON format
+ * node_serr.h - (De)Serialize a POD into a JSON format
  * Author: FallAngel
  * Date: 2021-08-09
  * 
  * Copyright (c) 2021
  */
 /**
- * (De)Serialize the `file_info` structure (see file_info.h for more information)
+ * (De)Serialize the `node` structure (see node.h for more information)
  * into a JSON file format.
  * 
  * E.g:
@@ -17,8 +17,8 @@
  * }
 */
 
-#ifndef _JSON_SERIALIZATION
-#define _JSON_SERIALIZATION
+#ifndef _NODE_SERR_H
+#define _NODE_SERR_H
 
 #include "../node.h"
 
@@ -33,16 +33,16 @@ struct Node* nodeDeserialize(const char *json, struct Node *_node, uint8_t _flag
 
 
 /**
- * `jsonWriteFile` load the `_node` structure into a JSON file.
- * And write the new filename into `_file_name` a malloc'd string.
+ * `jsonWriteFile` load the `node` structure into a JSON file.
+ * And write the new filename into `file_name` a malloc'd string.
 */
 int jsonWriteFile(char **_file_name, struct Node *_node, uint8_t _flags);
 
 /**
- * `jsonReadFile` read a JSON file indicated by `_file_name`
- *  and fill the `_node` structure. Return NULL on error
+ * `jsonReadFile` read a JSON file indicated by `file_name`
+ *  and fill the `node` structure. Return NULL on error
  *  and a pointer to the JSON content.
  */
 char* jsonReadFile(const char *_file_name, struct Node *_node, uint8_t _flags);
 
-#endif /* _JSON_SERIALIZATION */
+#endif /* _NODE_SERR_H */
